aa0f65ccbeabd65da1b3b33b3e891e055ae011d8
Ruslan Salikhov <ruslan.salihov@gmail.com>
2018-11-23 14:45:42 +0500

    Change market order

--- app/branding.js
+++ app/branding.js
@@ -77,7 +77,7 @@ export function getUnits(chainId = "4018d784") {
  */
 
 export function getMyMarketsBases() {
-    return ["BTS", "RUBLE", "USD", "RUDEX.BTC", "RUDEX.EOS", "CNY"];
+    return ["BTS", "RUDEX.BTC", "RUDEX.EOS", "RUBLE", "USD", "CNY"];
 }
 
 /**
